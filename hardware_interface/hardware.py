920e3d63653cec3b24cc498be7c59a89c56952d2
Gray Rybka <grybka@uw.edu>
2019-06-18 16:30:24 -0700

    added led strip

--- hardware_interface/hardware.py
+++ hardware_interface/hardware.py
@@ -1,5 +1,6 @@
 import Adafruit_PCA9685
 import RPi.GPIO as GPIO
+from rpi_ws281x import *
 import logging
 
 class GratbotServo:
@@ -109,6 +110,25 @@ class GratbotIRSensor:
     def get_status(self):
         return GPIO.input(self.my_pin)
 
+class GratBotLEDStrip:
+    def __init__(self,datastruct):
+        LED_COUNT      = 12      # Number of LED pixels.
+        LED_PIN        = 12      # GPIO pin connected to the pixels (18 uses PWM!).
+        #LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+        LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+        LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+        LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+        LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+        LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+        self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+        self.strip.begin()
+
+    def set_all_color(color):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i,color)
+        strip.show()
+
+    def set_pixel_color(p,color):
         
 def create_hardware(datastruct):
     hardware_dat={}
@@ -123,6 +143,8 @@ def create_hardware(datastruct):
                 hardware_dat[x]=GratbotMotor(datastruct[x])
             elif datastruct[x]["type"]=="GratbotIRSensor":
                 hardware_dat[x]=GratbotIRSensor(datastruct[x])
+            elif datastruct[x]["type"]=="GratbotLEDStrip":
+                hardware_dat[x]=GratbotLEDStrip(datastruct[x])
             else:
                 logging.warning("Unrecognized hardware type {}".format(datastruct[x]["type"]))
     return hardware_dat
