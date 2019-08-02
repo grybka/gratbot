1173ddaae5181757bc9469c1d42cc84346128b52
Gray Rybka <grybka@uw.edu>
2019-08-02 16:56:05 -0700

    added controller support to dockerfile

--- camera/start.sh
+++ camera/start.sh
@@ -5,6 +5,8 @@ xhost +$IP
 #docker run --name gratbot_camera --rm -v /Users/grybka/projects/gratbot/camera/app:/app -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$IP:0 -p 5000:5000 -p 8888:8888 -it  grat_opencv:latest /bin/bash
 myapp=$PWD/app
 echo "myapp " $myapp
-docker run --name gratbot_camera --rm -v $myapp:/app -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$IP:0 -p 5000:5000 -p 8888:8888 -it  grat_opencv:latest /bin/bash
+#docker run --name gratbot_camera --rm -v $myapp:/app -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$IP:0 -p 5000:5000 -p 8888:8888 --privileged -v /dev/input/js0:/dev/input/js0 -it  grat_opencv:latest /bin/bash
+docker run --name gratbot_camera --rm -v $myapp:/app -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$IP:0 -p 5000:5000 -p 8888:8888 --device=/dev/input/js0 -v /sys/class/leds/xpad0/brightness:/sys/class/leds/xpad0/brightness  -it  grat_opencv:latest /bin/bash
+#docker run --name gratbot_camera --rm -v $myapp:/app -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$IP:0 -p 5000:5000 -p 8888:8888 -it  grat_opencv:latest /bin/bash
 
 #docker run --name gratbot_camera --rm -v /tmp/.X11-unix:/tmp/.X11-unix -v /Users/grybka/projects/gratbot/camera/app:/app -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$IP:0 -p 5000:5000 -p 8888:8888 -it spmallick/opencv-docker:opencv /bin/bash
