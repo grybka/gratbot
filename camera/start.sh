90ff8c62d0ccd6a01dfe72452a2ff07d72dec6a7
Gray Rybka <grybka@uw.edu>
2019-08-04 21:53:27 -0700

    can now drive around with controller

--- camera/start.sh
+++ camera/start.sh
@@ -1,12 +1,17 @@
 #starts a docker image that will be able to display to an osx screen
 #you'll need to run "workon workon OpenCV-master-py3" after starting the container to access opencv
-IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
-xhost +$IP
+#IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+#xhost +$IP
+xhost +local:root
 #docker run --name gratbot_camera --rm -v /Users/grybka/projects/gratbot/camera/app:/app -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$IP:0 -p 5000:5000 -p 8888:8888 -it  grat_opencv:latest /bin/bash
 myapp=$PWD/app
 echo "myapp " $myapp
 #docker run --name gratbot_camera --rm -v $myapp:/app -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$IP:0 -p 5000:5000 -p 8888:8888 --privileged -v /dev/input/js0:/dev/input/js0 -it  grat_opencv:latest /bin/bash
-docker run --name gratbot_camera --rm -v $myapp:/app -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$IP:0 -p 5000:5000 -p 8888:8888 --device=/dev/input/js0 -v /sys/class/leds/xpad0/brightness:/sys/class/leds/xpad0/brightness  -it  grat_opencv:latest /bin/bash
+#docker run --name gratbot_camera --rm -v $myapp:/app -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$IP:0 -p 5000:5000 -p 8888:8888 --device=/dev/input/js0 -v /sys/class/leds/xpad0/brightness:/sys/class/leds/xpad0/brightness  -it  grat_opencv:latest /bin/bash #THIS IS FOR THE MAC
+docker run --name gratbot_camera --rm -v $myapp:/app -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --device=/dev/input/js0 -v /sys/class/leds/xpad0/brightness:/sys/class/leds/xpad0/brightness  -it  grat_opencv:latest /bin/bash
+
+
+
 #docker run --name gratbot_camera --rm -v $myapp:/app -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$IP:0 -p 5000:5000 -p 8888:8888 -it  grat_opencv:latest /bin/bash
 
 #docker run --name gratbot_camera --rm -v /tmp/.X11-unix:/tmp/.X11-unix -v /Users/grybka/projects/gratbot/camera/app:/app -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$IP:0 -p 5000:5000 -p 8888:8888 -it spmallick/opencv-docker:opencv /bin/bash
