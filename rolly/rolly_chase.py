e27d9226eea01434d23a6f87323fb51889f5dc42
Gray Rybka <gyrbka@uw.edu>
2020-08-11 12:23:18 -0700

    chase update

--- rolly/rolly_chase.py
+++ rolly/rolly_chase.py
@@ -14,16 +14,32 @@ class RollyChase(DisplayCamera):
         self.next_action_time=time.time()+self.wait_duration_seconds
         self.video_objects=None
 
+    def get_top_object(self,video_objects,object_class,min_confidence):
+        best_obj=None
+        for obj in video_objects:
+            if obj["label"]==object_class and obj["confidence"]>min_confidence:
+                if best_obj==None:
+                    best_obj=obj
+                else:
+                    if obj["confidence"]>best_obj["confidence"]:
+                        best_obj=obj
+        return best_obj
 
-    def decide(self,video_objects,video_frame):
-        #find what I want to trak
+    def tag_objects(self,video_frame):
+        if self.yv5model==None:
+            self.yv5model=yolov5_tool()
+                #self.yv5model.initialize("E:/projects/yolov5/runs/exp9/weights/last.pt")
+            self.yv5model.initialize("C:/Users/grybk/projects/yolov5/yolov5/runs/exp28/weights/last.pt")
+        video_objects=get_video_frame_objects_yolov5(video_frame,self.yv5model)
+        draw_object_bboxes(video_frame,video_objects)
+        return video_objects,video_frame
 
+    def act_on_objects(self,to_track):
         to_track=None
         to_track=self.get_top_object(video_objects,"purple_ball",0.2)
         if to_track==None:
             if self.tracking==True:
                 #this means I lost it
-                cv.imwrite(time.strftime("%Y%m%d-%H%M%S.jpg"),video_frame)
                 self.tracking=False
             return
         self.tracking=True
@@ -61,7 +77,12 @@ class RollyChase(DisplayCamera):
             self.actions.append([ [ "leg_controller","right_speed", "SET" ], leg_motion_r])
 
     def act(self):
-        #return self.show_video()
-        now=time.time()
-        if now<self.next_action_time:
-            return None
+        image=self.get_image()
+        if image is not None: #run decide for each new image
+            video_objects,image=self.tag_objects(image)
+            to_track=self.get_top_object(video_objects,"purple_ball",0.2)
+            #cv.imwrite(time.strftime("%Y%m%d-%H%M%S.jpg"),video_frame)
+            if to_track is not None:
+                self.act_on_objects(to_track)
+            return image
+        return None
