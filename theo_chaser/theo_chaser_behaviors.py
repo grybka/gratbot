c863ed109086caf6effd2a56498be2d5370c09b7
Gray Rybka <grybka@uw.edu>
2020-10-07 16:22:28 -0700

    added theo chaser

---
+++ theo_chaser/theo_chaser_behaviors.py
@@ -0,0 +1,59 @@
+import time
+import cv2 as cv
+import base64
+import numpy as np
+
+class GratbotBehavior:
+    def __init__(self, comms):
+        self.comms = comms
+    def act(self):
+        return
+    def shut_down(self):
+        return
+
+class DisplayCamera(GratbotBehavior):
+    def __init__(self, comms):
+        super().__init__(comms)
+        self.onstate="start"
+        self.rollstate="forward"
+        self.last_roll_timestamp=time.time()
+        self.roll_interval=3
+        self.wheel_speed=40.0
+
+        return
+
+    def act(self):
+        return self.get_image()
+
+    def roll(self):
+        if time.time()>self.last_roll_timestamp+self.roll_interval:
+            if self.rollstate=="forward":
+                self.last_roll_timestamp=time.time()
+                self.comms.set_intention( ["wheel_motor","speed","SET" ], self.wheel_speed)
+                self.rollstate="backward"
+            else:
+                self.last_roll_timestamp=time.time()
+                self.comms.set_intention( ["wheel_motor","speed","SET" ], -self.wheel_speed )
+                self.rollstate="forward"
+
+    def get_image(self):
+        #returns an image if a new one is available
+        #otherwise none if waiting for one
+        if self.onstate=="start":
+            #send a request for a new image
+            self.image_request_timestamp=time.time()
+            self.comms.set_intention( ["camera","image","GET" ], 1 )
+            self.onstate="waiting"
+            return None
+        elif self.onstate=="waiting":
+            image,image_timestamp=self.comms.get_state("camera/image/GET")
+            #print("polling: {}".format(image_timestamp))
+            if image_timestamp is not None and image_timestamp>self.image_request_timestamp:
+                #print("received frame after {} seconds".format(image_timestamp-self.image_request_timestamp))
+                x=bytes(image,encoding='utf-8')
+                x=base64.b64decode(x)
+                x=np.frombuffer(x,dtype=np.uint8)
+                myframe=cv.imdecode(x,cv.IMREAD_COLOR)
+                self.onstate="start"
+                return myframe
+        return None
