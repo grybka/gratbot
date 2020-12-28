b5dd44c3c67eedc38855812141503ef8b9d6c2c1
Gray Rybka <grybka@uw.edu>
2020-12-28 13:25:54 -0800

    added another sonic test

---
+++ tests/test_ultrasonic2.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('../hardware_interface')
+import yaml
+import logging
+import hardware
+import leg_control
+
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+
+
+#initialize hardware
+config_file=open("../hardware_interface/mecanum_hardware_config.yaml","r")
+config_data=yaml.safe_load(config_file)
+config_file.close()
+robot=hardware.create_hardware(config_data["hardware"])
+robot_thread=hardware.GratbotHardwareThread(robot)
+
+while True:
+    if a=="q":
+        break
+    time.sleep(1)
+    print(robot["ultrasonic_sensor"].get("anything"))
