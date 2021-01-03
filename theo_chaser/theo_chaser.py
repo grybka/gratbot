15e13b07b41f928e8469a2ec25e2564ecaf6a626
Gray Rybka <grybka@uw.edu>
2021-01-02 19:59:28 -0800

    added usonic sensor frequency

--- theo_chaser/theo_chaser.py
+++ theo_chaser/theo_chaser.py
@@ -13,7 +13,7 @@ sys.path.append('../gratbot_client')
 from GratbotComms import GratbotComms
 from GratbotClient import GratbotClient
 from theo_chaser_manualbehavior import XBoxControl
-from theo_chaser_chase import Theo_Chaser_Chase
+from theo_chaser_chase import Theo_Chaser_Chase_MK2
 from theo_chaser_line_follower import Theo_Chaser_Line_Follower
 from theo_chaser_learn_and_track import Theo_Chaser_Learn_And_Track
 from theo_chaser_mapper import Theo_Chaser_Mapper
@@ -35,9 +35,9 @@ keep_going=True
 #on_behavior=DisplayCamera(gratbot_comms)
 #on_behavior=XBoxControl(gratbot_comms)
 #on_behavior=Theo_Chaser_Learn_And_Track(gratbot_comms)
-on_behavior=Theo_Chaser_Mapper(gratbot_comms)
+#on_behavior=Theo_Chaser_Mapper(gratbot_comms)
 #on_behavior=Theo_Chaser_Line_Follower(gratbot_comms)
-#on_behavior=Theo_Chaser_Chase(gratbot_comms)
+on_behavior=Theo_Chaser_Chase_MK2(gratbot_comms)
 #on_behavior=RollyChase(gratbot_comms)
 
 def shut_down():
