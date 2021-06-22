40b19988c8d9d346cbb46252f4fbac284842f180
Gray Rybka <grybka@uw.edu>
2021-06-22 13:18:26 -0700

    handled nones in bno055

--- gratbot_server_refactor/hardware/BNO055_detailed.py
+++ gratbot_server_refactor/hardware/BNO055_detailed.py
@@ -3,6 +3,7 @@ import time
 import board
 import busio
 import adafruit_bno055
+import threading
 from collections import deque
 import numpy as np
 
@@ -34,9 +35,15 @@ class Gratbot9DOFSensor(GratbotSpimescape):
         while not self.end_called:
             time.sleep(0.01)
             with self.data_lock:
-                self.magdeque.appendleft(self.sensor.magnetic)
-                self.accel_deque.appendleft(self.sensor.linear_acceleration)
-                self.gyro_deque.appendleft(self.sensor.gyro)
+                mag=self.sensor.magnetic
+                accel=self.sensor.linear_acceleration
+                gyro=self.sensor.gyro
+                if None not in mag:
+                    self.mag_deque.appendleft(mag)
+                if None not in accel:
+                    self.accel_deque.appendleft(accel)
+                if None not in gyro:
+                    self.gyro_deque.appendleft(gyro)
 
     def __del__(self):
         self.end_called=True
@@ -47,10 +54,11 @@ class Gratbot9DOFSensor(GratbotSpimescape):
 
     def get_update(self,last_time): #request changes since time as dictionary
         ret={}
-        with self.data_dock:
+        with self.data_lock:
             ret["b_field"]=(np.sum(self.mag_deque,axis=0)/self.deque_size).tolist()
             ret["b_field_stdev"]=np.std(self.mag_deque,axis=0).tolist()
             ret["b_field_inst"]=self.mag_deque[0]
+            #print("accel deque {}".format(self.accel_deque))
             ret["acceleration"]=(np.sum(self.accel_deque,axis=0)/self.deque_size).tolist()
             ret["acceleration_stdev"]=np.std(self.accel_deque,axis=0).tolist()
             ret["acceleration_inst"]=self.accel_deque[0]
