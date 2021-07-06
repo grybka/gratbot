acdb7efd91a7fa7c36a1f2358cb2ed842ed744e9
Gray Rybka <grybka@uw.edu>
2021-07-06 10:12:32 -0700

    network works

--- gratbotmk4/gyrii/underpinnings/JSONBackAndForthServer.py
+++ gratbotmk4/gyrii/underpinnings/JSONBackAndForthServer.py
@@ -5,15 +5,16 @@ import threading,queue
 import logging
 import sys, time
 import select
+#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARNING)
 
 class JSONBackAndForth():
 
     def __init__(self,debug=False):
         self.debug=debug
         self.sock=None
-        self.thread = threading.Thread(target=self._thread_loop)
+        self.server_sock=None
         self.should_quit = False
-        self.thread.daemon = True
         self.input_queue=queue.Queue()
         self.output_queue=queue.Queue()
 
@@ -21,110 +22,137 @@ class JSONBackAndForth():
         self.host=host
         self.port=port
         #connect to server
-        if self.debug:
-            print("creating client socket")
+        logging.debug("creating client socket")
         self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        if self.debug:
-            print("connecting")
+        self.sock.settimeout(5)
+        logging.debug("connecting")
         conninfo= self.sock.connect((self.host,self.port))
+        logging.debug("starting thread")
+
+        self.thread = threading.Thread(target=self._thread_loop)
+        self.thread.daemon = True
         self.thread.start()
 
+    def join(self):
+        self.should_quit=True
+        self.thread.join()
+
     def start_server(self,port):
-        if self.debug:
-            print("creating server")
-        self.sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        logging.debug("creating server socket")
+        self.server_sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_sock.settimeout(5)
         self.port=port
         self.host = socket.gethostname()
         self.host = "10.0.0.4"
-        if self.debug:
-            print("binding to {} {}".format(self.host,port))
-        self.sock.bind((self.host, port))
-        if self.debug:
-            print("listening")
-        self.sock.listen(1) #accept only one connection at a time
-        if self.debug:
-            print("accepting")
-        (clientsocket, address) = self.sock.accept()
-        if self.debug:
-            print("thread starting")
+        logging.debug("binding to {} {}".format(self.host,port))
+        self.server_sock.bind((self.host, port))
+        logging.debug("listening")
+        self.server_sock.listen(1) #accept only one connection at a time
+        logging.debug("thread starting")
+        self.thread = threading.Thread(target=self._thread_loop_server)
+        self.thread.daemon = True
         self.thread.start()
 
-    def _thread_loop(self):
+    def _thread_loop_server(self):
         while not self.should_quit:
-            inputs=[self.sock]
-            outputs=[self.sock]
-            print("calling select")
-            readable, writable, exceptional = select.select(inputs, outputs, inputs)
-            print("select released")
-            print("lenngtsh from select {},{},{}".format(len(readable),len(writable),len(exceptional)))
-            if self.sock in exceptional:
-                logging.error("Exception in socket")
-            if self.sock in readable:
-                #self.data = self.rfile.readline().strip()
-                #first receive number of bytes message is
-                if self.debug:
-                    print("reading")
+            logging.debug("accepting")
+            try:
+                (self.sock, address) = self.server_sock.accept()
+                self._thread_loop()
+            except socket.timeout:
+                ... #this is fine, just retry
+            except:
+                logging.warning("unhandled exception in accept, closing server")
+                self.should_quit=True
+        self.server_sock.close()
 
-                header=self.sock.recv(8)
 
-                length=int.from_bytes(header,byteorder='big')
-                if self.debug:
-                    print("heading length is {}".format(length))
+    def _thread_loop(self):
+        with self.sock:
+            while not self.should_quit:
+                inputs=[self.sock]
+                outputs=[self.sock]
+                #logging.debug("select")
+                readable, writable, exceptional = select.select(inputs, outputs, inputs,5) #timeout 5 seconds
+                #logging.debug("select released")
+                if self.sock in exceptional:
+                    logging.error("Exception in socket")
+                if self.sock in readable:
+                    #self.data = self.rfile.readline().strip()
+                    #first receive number of bytes message is
+                    logging.debug("reading")
 
-                counter=0
-                buf=b''
-                while(counter<length):
-                    buf+=self.sock.recv(length-counter)
-                if buf==b'':
-                    logging.info("Closing connection to ".format(self.host))
-                    break
-                try:
-                    datastructure=json.loads(buf.decode('utf-8'))
-                    self.input_queue.put(datastructure)
-                except Exception as error:
-                    logging.error("Error parsing json")
-                    logging.exception(error)
-            if self.sock in writable: 
-                if not self.output_queue.empty():
-                    tosend=bytes(json.dumps(self.output_queue.get())).encode()
-                    header=len(tosend).to_bytes(8,byteorder='big').encode()
-                    if self.debug:
-                        print("writing {} and {}".format(header,tosend))
-                    self.sock.send(header)
-                    self.sock.send(tosend)
-                    #self.wfile.write(bytes(json.dumps(self.output_queue.get())+"\n",encoding='utf-8'))
-                else:
-                    print("I could send, but have nothing to say")
+                    data=self.sock.recv(1024)
+                    logging.debug("got data {}".format(data))
+                    if data==b'':
+                        logging.info("Closing connection to ".format(self.host))
+                        break
+                    while data[-1]!=10:
+                        logging.debug("data end character is {}".format(int(data[-1])))
+                        newdata=self.sock.recv(1024)
+                        data+=newdata
+                        logging.debug("got data {}".format(data))
+                        if newdata==b'':
+                            logging.warning("Connection broken ".format(self.host))
+                            break
+
+                    try:
+                        json_strings=data.decode().split('\n') #andle multiple messages all in one go
+                        json_strings.pop(-1)
+                        for s in json_strings:
+                            logging.debug("message is {}".format(s))
+                            datastructure=json.loads(s+'\n')
+                            self.input_queue.put(datastructure)
+                    except Exception as error:
+                        logging.error("Error parsing json")
+                        logging.exception(error)
+                if self.sock in writable: 
+                    if not self.output_queue.empty():
+                        logging.debug("writing")
+                        self.sock.sendall((json.dumps(self.output_queue.get())+"\n").encode())
+                    else:
+                        time.sleep(0.001) #just for a break
+            logging.warning("closing socket")
+            self.sock.close()
 
 if __name__ == '__main__':
-    test_port=23032
+    test_port=23033
     if sys.argv[1]=='server':
         server=JSONBackAndForth(debug=True)
         print("starting server")
         server.start_server(test_port)
         print("server started")
         last_send=0
-        while True:
-            time.sleep(0.001)
-            if time.time()>last_send+1:
-                server.output_queue.put({"body": "from server"})
-                last_send=time.time()
-                #print("server sends")
-            if not server.input_queue.empty():
-                print("message receieved: ".format(server.input_queue.get()))
+        try:
+            while True:
+                time.sleep(0.01)
+                if time.time()>last_send+1:
+                    server.output_queue.put({"body": "from server"})
+                    last_send=time.time()
+                    #print("server sends")
+                if not server.input_queue.empty():
+                    print("message receieved: {} ".format(server.input_queue.get()))
+        except KeyboardInterrupt:
+            logging.error("Keyboard interrupt")
+            server.join()
+
     elif sys.argv[1]=='client':
-        client=JSONBackAndForth()
-        #client.start_client('localhost',23033)
-        client.start_client("10.0.0.4",test_port)
-        print("client started")
-        last_send=0
-        while True:
-            time.sleep(0.001)
-            if time.time()>last_send+1.15:
-                client.output_queue.put({"body": "from client"})
-                last_send=time.time()
-                #print("client sends")
-            if not client.input_queue.empty():
-                print("message receieved: ".format(client.input_queue.get()))
+        try:
+            client=JSONBackAndForth()
+            #client.start_client('localhost',23033)
+            client.start_client("10.0.0.4",test_port)
+            print("client started")
+            last_send=0
+            while True:
+                time.sleep(0.01)
+                if time.time()>last_send+1.15:
+                    client.output_queue.put({"body": "from client"})
+                    last_send=time.time()
+                    #print("client sends")
+                if not client.input_queue.empty():
+                    print("message receieved: {}".format(client.input_queue.get()))
+        except KeyboardInterrupt:
+            logging.error("Keyboard interrupt")
+            client.join()
     else:
         print("don't know how to {}".format(sys.argv[1]))
