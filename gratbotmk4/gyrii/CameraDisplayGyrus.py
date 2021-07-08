ffd57bbb9a580f9a2e577872ad30481b8af31e0b
Gray Rybka <grybka@uw.edu>
2021-07-07 21:54:56 -0700

    camera display with fps

--- gratbotmk4/gyrii/CameraDisplayGyrus.py
+++ gratbotmk4/gyrii/CameraDisplayGyrus.py
@@ -1,10 +1,16 @@
 
 from Gyrus import ThreadedGyrus
-import logging
+import logging,time
+import cv2
 
 class CameraDisplayGyrus(ThreadedGyrus):
     def __init__(self,broker,display=None):
         self.display=display
+        self.show_fps=True
+        self.fps=0
+        self.fps_count=11
+        self.fps_count_reset=10
+        self.fps_start_time=0
         super().__init__(broker)
 
     def get_keys(self):
@@ -14,4 +20,14 @@ class CameraDisplayGyrus(ThreadedGyrus):
         return "CameraDisplayGyrus"
 
     def read_message(self,message):
-        self.display.update_image("camera",message["image"])
+        if "image" in message:
+            frame=message["image"]
+            if self.show_fps==True:
+                self.fps_count+=1
+                if self.fps_count>self.fps_count_reset:
+                    self.fps=self.fps_count_reset/(time.time()-self.fps_start_time)
+                    self.fps_start_time=time.time()
+                    self.fps_count=0
+                color = (255, 0, 0)
+                cv2.putText(frame, "NN fps: {:.2f}".format(self.fps), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color)
+            self.display.update_image("camera",message["image"])
