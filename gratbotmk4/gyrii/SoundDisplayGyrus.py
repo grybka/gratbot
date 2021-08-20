4cb2fae543f8df77b4eb2fd666d5708cc523a8bd
Gray Rybka <grybka@uw.edu>
2021-08-19 21:29:09 -0700

    sound display works

--- gratbotmk4/gyrii/SoundDisplayGyrus.py
+++ gratbotmk4/gyrii/SoundDisplayGyrus.py
@@ -2,6 +2,8 @@
 from Gyrus import ThreadedGyrus
 import logging,time
 import cv2
+import numpy as np
+import array
 logger=logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
@@ -19,13 +21,15 @@ class SoundDisplayGyrus(ThreadedGyrus):
     def read_message(self,message):
         if "microphone_data" in message:
             data=message["microphone_data"]
+            data = array.array('h', data)
             ps = np.abs(np.fft.fft(data))**2
-            yscale=np.max(ps)
+            ps=ps[0:int(len(ps)/2)]
+            yscale=np.max(ps)+1
             #time_step=1/16000
             #freqs = np.fft.fftfreq(len(data), time_step)
-            image=np.zeros(200,320,3,dtype=np.uint8)
+            image=np.zeros([200,320,3],dtype=np.uint8)
             for i in range(len(ps)):
                 xpix=int(320*i/len(ps))
-                ypix=int(100*ps[i]/yscale)
+                ypix=199-int(200*ps[i]/yscale)
                 image[ypix,xpix,:]=np.array([255,255,255])
             self.display.update_image("Microphone",image)
