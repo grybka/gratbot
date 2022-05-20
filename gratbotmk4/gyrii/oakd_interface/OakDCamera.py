3d40162f138ccc1008054399eac3710672f69d58
Gray Rybka <grybka@uw.edu>
2022-05-20 16:00:48 -0700

    try flipped

--- gratbotmk4/gyrii/oakd_interface/OakDCamera.py
+++ gratbotmk4/gyrii/oakd_interface/OakDCamera.py
@@ -103,6 +103,6 @@ class OakDDepth(OakDElement):
                 frame_message={"timestamp": time.time()}
                 image_timestamp=inDepth.getTimestamp().total_seconds()
                 frame_message["image_timestamp"]=image_timestamp
-                frame_message["depth_image"]=cv2.resize(frame,(100,160) )
+                frame_message["depth_image"]=cv2.resize(frame,(160,100) )
                 frame_message["keys"]=["depth"]
                 broker.publish(frame_message,frame_message["keys"])
