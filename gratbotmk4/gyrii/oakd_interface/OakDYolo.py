adf0aea20aa0d8d4bba76aa8b02cb875a71d253a
Gray Rybka <grybka@uw.edu>
2022-05-21 11:22:29 -0700

    added ok labels to yolo

--- gratbotmk4/gyrii/oakd_interface/OakDYolo.py
+++ gratbotmk4/gyrii/oakd_interface/OakDYolo.py
@@ -32,6 +32,7 @@ labelMap = [
 class OakDYoloDetections(OakDElement):
     def __init__(self,pipeline,model_name,shaves,camera,stereo,streamname,model_labels,confidence_threshold=0.3,spatial_x_scale=1.0):
         logger.info("Creating Spatial Detection Network")
+        self.ok_labels=[labelMap.index("person"),labelMap,index("sports ball")]
         self.spatial_x_scale=spatial_x_scale #for cases where I've squeezed the resolution
         self.streamname=streamname
         #spatialDetectionNetwork = pipeline.createYoloSpatialDetectionNetwork()
@@ -68,6 +69,8 @@ class OakDYoloDetections(OakDElement):
             device_timestamp=inDet.getTimestamp().total_seconds()
             detection_message=[]
             for detection in inDet.detections:
+                if detection.label not in self.ok_labels:
+                    continue
                 det_item={}
                 bbox_array=[detection.xmin,detection.xmax,detection.ymin,detection.ymax]
                 det_item["bbox_array"]=bbox_array
