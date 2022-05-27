28e613e621f46d2bd7e56fca66631782468679a3
Gray Rybka <grybka@uw.edu>
2022-05-27 13:22:31 -0700

    new speed

--- gratbotmk4/gyrii/SpeedTestGyrus.py
+++ gratbotmk4/gyrii/SpeedTestGyrus.py
@@ -5,6 +5,7 @@ import logging
 import uuid
 from Gyrus import ThreadedGyrus
 from oakd_interface.OakDCalculations import HostSpatialsCalc
+from underpinnings.LocalPositionLog import LocalPositionLog
 logger=logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
@@ -12,6 +13,7 @@ class SpeedTestGyrus(ThreadedGyrus):
     def __init__(self,broker):
         super().__init__(broker)
         self.spatial_calc=HostSpatialsCalc(71.86)
+        self.position_log=LocalPositionLog()
         self.depthFrame=None
 
     def get_keys(self):
@@ -21,17 +23,29 @@ class SpeedTestGyrus(ThreadedGyrus):
         return "SpeedTestGyrus"
 
     def read_message(self,message):
+        self.position_log.read_message(message)
         if "depth_image" in message:
             self.depthFrame=message["depth_image"]
         if "detections" in message and len(message["detections"])!=0:
-            if self.depthFrame is None:
-                return
             detections=message["detections"]
+            start=time.time()
+            quat=Quaternion(self.position_log.pointing)
+            rot=quat.rotation_matrix
             for i in range(len(detections)):
-                bbox_array=detections[i]["bbox_array"]
-                start=time.time()
-                logger.debug("bbox array {}".format(bbox_array))
-                depth=self.spatial_calc.calc_spatials( self.depthFrame, bbox_array, averaging_method=np.mean)
-                logging.debug("time to calculate depth {}".format(start-time.time()))
-                logging.debug("spatials are is {}".format(depth))
-                logging.debug("onboard spatials are {}".format(detections[i]["spatial_array"]))
+                yxz=detections[i]["spatial_array"]
+                xyz=np.array(yxz[1],yxz[0],yzx[2])
+                new_xyz=rot@xyz
+            logging.debug("time to calculate rotation {}".format(start-time.time()))
+
+
+            #if self.depthFrame is None:
+            #    return
+            #detections=message["detections"]
+            #for i in range(len(detections)):
+            #    bbox_array=detections[i]["bbox_array"]
+            #    start=time.time()
+            #    logger.debug("bbox array {}".format(bbox_array))
+            #    depth=self.spatial_calc.calc_spatials( self.depthFrame, bbox_array, averaging_method=np.mean)
+            #    logging.debug("time to calculate depth {}".format(start-time.time()))
+            #    logging.debug("spatials are is {}".format(depth))
+            #    logging.debug("onboard spatials are {}".format(detections[i]["spatial_array"]))
